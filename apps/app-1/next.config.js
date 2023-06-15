52779fa60fd48f5370ef87fb0c3677890403802d
052hide <hidetoshi1990@gmail.com>
2023-06-15 16:05:30 +0900

    add rewrites

--- apps/app-1/next.config.js
+++ apps/app-1/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/app2/:match*',
+        destination: 'https://vercel-multiple-app.vercel.app/:match*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
--- apps/app-2/next.config.js
+++ apps/app-2/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  assetPrefix: 'https://vercel-multiple-app.vercel.app/',
+}
 
 module.exports = nextConfig
